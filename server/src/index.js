534b6e8d549fd824bd1eb3ba80859d1c8ba0655a
zombizz <thibaultribes.tr@gmail.com>
2025-11-06 11:35:15 +0100

    change ci

--- server/src/index.js
+++ server/src/index.js
@@ -3,13 +3,15 @@ import express from 'express'
 import helmet from 'helmet'
 import mongoose from 'mongoose'
 import morgan from 'morgan'
+import path from 'path'
+// eslint-disable-next-line sort-imports
+import { fileURLToPath } from 'url'
 // eslint-disable-next-line sort-imports
 import authRouter from './routes/auth.js'
 import productsRouter from './routes/products.js'
 import restaurantsRouter from './routes/restaurants.js'
 // eslint-disable-next-line sort-imports
 import 'dotenv/config'
-import * as path from "node:path";
 
 const app = express()
 
@@ -26,6 +28,16 @@ app.get('/health', (req, res) => res.json({ ok: true }))
 app.use('/api/restaurants', restaurantsRouter)
 app.use('/api/products', productsRouter)
 app.use('/api/auth', authRouter)
+
+// Servez le frontend Vite (client/dist) et fallback SPA
+// eslint-disable-next-line one-var,no-underscore-dangle
+const __dirname = path.dirname(fileURLToPath(import.meta.url)),
+ distPath = path.resolve(__dirname, '../../client/dist')
+app.use(express.static(distPath))
+app.get('*', (req, res, next) => {
+  if (req.path.startsWith('/api')) {return next()}
+  return res.sendFile(path.join(distPath, 'index.html'))
+})
 
 // eslint-disable-next-line one-var,no-undef,no-magic-numbers
 const port = process.env.PORT || 4000
